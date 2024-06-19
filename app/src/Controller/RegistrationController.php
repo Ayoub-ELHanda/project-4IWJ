877692306f0715b4c7970b486e8034a0e2dfc1a4
ayoubmcw <elhandaayo@gmail.com>
2024-06-19 00:03:48 -1000

    fix error not fond

--- app/src/Controller/RegistrationController.php
+++ app/src/Controller/RegistrationController.php
@@ -73,6 +73,59 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+    #[Route('/oubli-pass', name:'forgotten_password')]
+    public function forgottenPassword(
+        Request $request,
+        UsersRepository $usersRepository,
+        TokenGeneratorInterface $tokenGenerator,
+        EntityManagerInterface $entityManager,
+        SendMailService $mail
+    ): Response
+    {
+        $form = $this->createForm(ResetPasswordRequestFormType::class);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            //On va chercher l'utilisateur par son email
+            $user = $usersRepository->findOneByEmail($form->get('email')->getData());
+
+            // On vérifie si on a un utilisateur
+            if($user){
+                // On génère un token de réinitialisation
+                $token = $tokenGenerator->generateToken();
+                $user->setResetToken($token);
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                // On génère un lien de réinitialisation du mot de passe
+                $url = $this->generateUrl('reset_pass', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+                
+                // On crée les données du mail
+                $context = compact('url', 'user');
+
+                // Envoi du mail
+                $mail->send(
+                    'no-reply@e-commerce.fr',
+                    $user->getEmail(),
+                    'Réinitialisation de mot de passe',
+                    'password_reset',
+                    $context
+                );
+
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');
+            }
+            // $user est null
+            $this->addFlash('danger', 'Un problème est survenu');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('security/reset_password_request.html.twig', [
+            'requestPassForm' => $form->createView()
+        ]);
+    }
+
 
 
 }
