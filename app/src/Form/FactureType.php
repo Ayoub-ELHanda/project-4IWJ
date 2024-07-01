6cdbf1239776762f43346fd502739ac233efa120
chems <chems917@gmail.com>
2024-07-01 11:48:12 +0200

    modif

--- app/src/Form/FactureType.php
+++ app/src/Form/FactureType.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Form;
+
 use App\Entity\Devis;
 use App\Entity\User;
 use App\Entity\Facture;
@@ -27,13 +28,20 @@ class FactureType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('mail', TextType::class)
-            ->add('nomClient', TextType::class)
-            ->add('telephone', TextType::class)
+            ->add('mail', TextType::class, [
+                'attr' => ['class' => 'px-3 py-2.5 mt-2.5 italic font-extralight bg-white rounded-md border border-black border-solid w-full'],
+            ])
+            ->add('nomClient', TextType::class, [
+                'attr' => ['class' => 'px-3 py-2.5 mt-2.5 italic font-extralight bg-white rounded-md border border-black border-solid w-full'],
+            ])
+            ->add('telephone', TextType::class, [
+                'attr' => ['class' => 'px-3 py-2.5 mt-2.5 italic font-extralight bg-white rounded-md border border-black border-solid w-full'],
+            ])
             ->add('user', TextType::class, [
                 'disabled' => true,
                 'data' => $this->getUserGarageName(),
                 'required' => false,
+                'attr' => ['class' => 'px-3 py-2.5 mt-2.5 italic font-extralight bg-white rounded-md border border-black border-solid w-full'],
             ])
             ->add('produits', EntityType::class, [
                 'class' => Produit::class,
@@ -43,15 +51,23 @@ class FactureType extends AbstractType
                 'multiple' => true,
                 'expanded' => true,
                 'required' => true,
+                'attr' => [
+                    'class' => 'flex flex-wrap gap-4 mt-2.5 mb-4',
+                ],
+                'choice_attr' => function(Produit $produit, $key, $value) {
+                    return ['class' => 'form-checkbox h-5 w-5 text-blue-600'];
+                },
             ])
             ->add('totalPrix', MoneyType::class, [
-                'label' => 'Total Prix des Produits',
-                'currency' => 'EUR',
+                'currency' => false,
                 'mapped' => false,
                 'disabled' => true,
                 'attr' => [
                     'readonly' => true,
+                    'class' => 'hidden',
                 ],
+                'label' => false, // Pas de label
+                'required' => false, // Pas requis
             ]);
 
         // Event listener for calculating totalPrix on form pre-set data
